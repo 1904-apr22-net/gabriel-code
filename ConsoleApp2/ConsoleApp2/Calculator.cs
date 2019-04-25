fdac69cbaf309dc2837227072613348d3c1afb38
gabrielgon37 <gabriel37gonzalez15@gmail.com>
2019-04-25 12:29:26 -0500

    Remove ignored files

---
+++ ConsoleApp2/ConsoleApp2/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Calculator
+    {
+        public void calc(int n)
+        {
+            
+            for (int x = n; x > 0; x--)
+            {
+                if (x % 3 == 0)
+                {
+                    Console.WriteLine(x);
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            Calculator cal = new Calculator();
+            cal.calc(int.Parse(Console.ReadLine()));
+        }
+    }
+}
